b1630c55528c3acd900e2298132d37b1d81d9ca2
Gabe Conradi <gabe.conradi@gmail.com>
2016-05-01 15:54:09 -0400

    add chromebook compat

--- bin/install.sh
+++ bin/install.sh
@@ -4,7 +4,10 @@ install_path=/usr/share/X11/xkb
 root="$(dirname $0)/.."
 
 echo "Installing keymap..."
-cp $root/symbols/de_byxorna $install_path/symbols/
+cp $root/symbols/{chromebook,de_byxorna} $install_path/symbols/
+
+echo "Installing compat..."
+cp $root/compat/chromebook $install_path/compat/
 
 echo "Installing rules..."
 cp $root/examples/rules/* $install_path/rules/
