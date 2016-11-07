32e10c5fa7f9b8f481b61345db3bc94027f6790a
Gabe Conradi <gabe.conradi@gmail.com>
2016-11-07 12:00:05 -0500

    hack around loss of overlay in libxkbcommon 0.6.1-1 for chromebook

--- bin/install.sh
+++ bin/install.sh
@@ -4,13 +4,13 @@ install_path=/usr/share/X11/xkb
 root="$(dirname $0)/.."
 
 echo "Installing keymap..."
-cp $root/symbols/{chromebook,de_byxorna} $install_path/symbols/
+cp $root/symbols/de_byxorna $install_path/symbols/
 
-echo "Installing compat..."
-cp $root/compat/chromebook $install_path/compat/
+#echo "Installing compat..."
+#cp $root/compat/chromebook $install_path/compat/
 
 echo "Installing rules..."
 cp $root/examples/rules/* $install_path/rules/
 
 echo "Installing xorg conf..."
-localectl --no-convert set-x11-keymap 'pc+de_byxorna(byxorna)+inet(evdev)+chromebook(overlay)' complete+chromebook
+localectl --no-convert set-x11-keymap 'pc+inet(evdev)+de_byxorna(byxorna)' complete
